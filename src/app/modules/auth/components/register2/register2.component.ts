e2a4ec13cb727d2d629a2a96a835ace444b69f15
bjakapong416 <boon.jakapong@gmail.com>
2022-03-27 23:31:52 +0700

    Register2 API and login-page

---
+++ src/app/modules/auth/components/register2/register2.component.ts
@@ -0,0 +1,147 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Subscription, Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { ConfirmPasswordValidator } from './confirm-password.validator';
+import { UserModel , User , Role } from '../../models/user.model';
+import { first } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-register2',
+  templateUrl: './register2.component.html',
+  styleUrls: ['./register2.component.scss']
+})
+export class Register2Component implements OnInit {
+
+  registrationForm: FormGroup;
+  hasError: boolean;
+  isLoading$: Observable<boolean>;
+
+  user : User;
+
+  // private fields
+  private unsubscribe: Subscription[] = []; // Read more: => https://brianflove.com/2016/12/11/anguar-2-unsubscribe-observables/
+
+  successmsg = false;
+  error = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.isLoading$ = this.authService.isLoading$;
+    // redirect to home if already logged in
+    if (this.authService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
+  }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  get f() {
+    return this.registrationForm.controls;
+  }
+
+  initForm() {
+    this.registrationForm = this.fb.group(
+      {
+        fullname: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(100),
+          ]),
+        ],
+        email: [
+          'qwe@qwe.qwe',
+          Validators.compose([
+            Validators.required,
+            Validators.email,
+            Validators.minLength(3),
+            Validators.maxLength(320), // https://stackoverflow.com/questions/386294/what-is-the-maximum-length-of-a-valid-email-address
+          ]),
+        ],
+        password: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(100),
+          ]),
+        ],
+        cPassword: [
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(100),
+          ]),
+        ],
+        agree: [false, Validators.compose([Validators.required])],
+      },
+      {
+        validator: ConfirmPasswordValidator.MatchPassword,
+      }
+    );
+  }
+
+  submit() {
+    this.hasError = false;
+    const result: {
+      [key: string]: string;
+    } = {};
+    Object.keys(this.f).forEach((key) => {
+      result[key] = this.f[key].value;
+    });
+    const newUser = new User();
+    newUser.setUser2(result);
+
+    console.log(result);
+    console.log(newUser);
+
+    const registrationSubscr = this.authService.singupUser(newUser)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.successmsg = true;
+          if (this.successmsg) {
+            // this.router.navigate(['/account/login']);
+            this.router.navigate(['/']);
+            
+          }
+        },
+        error => {
+          this.error = error ? error : '';
+          
+        }
+      )
+    
+
+    // const newUser = new UserModel();
+    // newUser.setUser(result);
+    // const registrationSubscr = this.authService
+    //   .registration(newUser)
+    //   .pipe(first())
+    //   .subscribe((user: UserModel) => {
+    //     if (user) {
+    //       this.router.navigate(['/']);
+    //     } else {
+    //       this.hasError = true;
+    //     }
+    //   });
+
+
+    // this.unsubscribe.push(registrationSubscr);
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe.forEach((sb) => sb.unsubscribe());
+  }
+
+
+}

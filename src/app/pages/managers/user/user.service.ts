62fbb90e59292561bd88bcd2f4da5b144c75c054
bjakapong416 <boon.jakapong@gmail.com>
2022-04-21 22:15:22 +0700

    Edit topbar and img profile

---
+++ src/app/pages/managers/user/user.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { timeout } from 'rxjs/operators';
+
+import { UserModels } from '../../_models/user.model';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { AuthService , User } from 'src/app/modules/auth';
+
+
+@Injectable({ providedIn: 'root' })
+export class userService {
+
+    readonly apiURL = `${environment.apiUrl}`;
+
+
+
+    user$: User | null = null;
+
+    // readonly Apiurl ="http://128.199.86.71:8000";
+    constructor(private httpClient: HttpClient) { }
+    model: any[] = [];
+
+
+
+    getUser1(): Observable<UserModels[]> {
+        const userProfiles = localStorage.getItem('currentUser$')
+
+        if(userProfiles != null){
+          this.user$ = JSON.parse(userProfiles) as User ;
+        }
+
+        const headers = new Headers({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${this.user$?.token}`
+          })
+
+        return this.httpClient.get<UserModels[]>(this.apiURL + '/listUsers'  ).pipe(
+          catchError(this.errorHandler)
+        )
+    }
+
+
+
+
+    errorHandler(error: any) {
+    let errorMessage = '';
+    if(error.error instanceof ErrorEvent) {
+        errorMessage = error.error.message;
+    } else {
+        errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+
+    return throwError(errorMessage);
+    }
+
+
+
+}

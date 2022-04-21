2edee59ac7e247f5ee8413e5501f6ea78dcfcd36
chunosuke <ch.chunspringz@gmail.com>
2022-04-21 17:12:35 +0700

    modal add cus not complete

--- src/app/pages/apps/customer/add-cus/add-cus.component.ts
+++ src/app/pages/apps/customer/add-cus/add-cus.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+//import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgbActiveModal, NgbDateAdapter, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { CustomerModels } from '../../../_models/customer.model';
+import { CustomerService } from '../../../_services/customer/customer.service';
 
 @Component({
   selector: 'app-add-cus',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-cus.component.scss']
 })
 export class AddCusComponent implements OnInit {
+  @Input() id: number;
+  mainData: CustomerModels;
+  //formGroup: FormGroup;
 
-  constructor() { }
+  //constructor(private customerService: CustomerService, private fb: FormBuilder, public modal: NgbActiveModal) { }
+  constructor(private customerService: CustomerService, public modal: NgbActiveModal) { }
 
   ngOnInit(): void {
+    this.FUNC_getDataById();
+  }
+
+  FUNC_getDataById() {
+    this.customerService.find(this.id).subscribe((data: any)=>{
+      this.mainData = data;
+      console.log(data);
+    })
   }
 
 }

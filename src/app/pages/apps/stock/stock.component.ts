9dfd392b136d64487b047868115d046d400c6c56
MiniPair <supakorn1497@gmail.com>
2022-04-30 17:09:34 +0700

    edit css stock page

--- src/app/pages/apps/stock/stock.component.ts
+++ src/app/pages/apps/stock/stock.component.ts
@@ -9,25 +9,46 @@ import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
-
 export class StockComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   filterValues: any = {};
   filterSelectObj: any = [];
 
-  displayedColumns: string[] = ['id', 'name', 'type', 'qty', 'unit', 'where', 'action'];
-  dispColumns: string[] = ['ID', 'Name', 'Type', 'Qty', 'Unit', 'Where', 'Action'];
-  fieldColumns: string[] = ['itemno', 'itemdes', 'itemgrp', 'qty', 'unitnam', 'whdes', 'itemno'];
+  displayedColumns: string[] = [
+    'name',
+    'type',
+    'qty',
+    'unit',
+    'where',
+    'action',
+  ];
+  dispColumns: string[] = [
+    'ID',
+    'Name',
+    'Type',
+    'Qty',
+    'Unit',
+    'Where',
+    'Action',
+  ];
+  fieldColumns: string[] = [
+    'itemno',
+    'itemdes',
+    'itemgrp',
+    'qty',
+    'unitnam',
+    'whdes',
+    'itemno',
+  ];
   dataSource = new MatTableDataSource();
-  dataSourceFilters = new MatTableDataSource()
+  dataSourceFilters = new MatTableDataSource();
   dataLength = 0;
 
-  p:number = 1;
+  p: number = 1;
   lowValue: number = 0;
   highValue: number = 10;
-
 
   // used to build a slice of papers relevant at any given time
   public getPaginatorData(event: any): void {
@@ -37,19 +58,25 @@ export class StockComponent implements OnInit {
   }
 
   mainDatas$: StockModels[] = [];
-  constructor(private route: ActivatedRoute, private authService: AuthService, private router: Router, public stockService: StockService) { 
+  constructor(
+    private route: ActivatedRoute,
+    private authService: AuthService,
+    private router: Router,
+    public stockService: StockService
+  ) {
     // Object to create Filter for
     this.filterSelectObj = [
       {
         name: 'Type',
         columnProp: 'itemgrp',
-        options: []
-      }, {
+        options: [],
+      },
+      {
         name: 'Unit',
         columnProp: 'unitnam',
-        options: []
-      }
-    ]
+        options: [],
+      },
+    ];
   }
 
   ngOnInit(): void {
@@ -61,7 +88,7 @@ export class StockComponent implements OnInit {
   }
 
   FUNC_getData() {
-    this.stockService.getAll().subscribe((data: any)=>{
+    this.stockService.getAll().subscribe((data: any) => {
       this.dataSource = new MatTableDataSource(data);
       this.dataSourceFilters = new MatTableDataSource(data);
       this.dataSource.paginator = this.paginator;
@@ -70,14 +97,15 @@ export class StockComponent implements OnInit {
       });
 
       this.dataSourceFilters.filterPredicate = this.createFilter();
-    })
+      console.log(this.dataSource.filteredData);
+    });
   }
 
-  FUNC_Delete(id: string){
-    this.stockService.delete(id).subscribe(res => {
-         this.mainDatas$ = this.mainDatas$.filter(item => item.itemno !== id);
-         console.log('Post deleted successfully!');
-    })
+  FUNC_Delete(id: string) {
+    this.stockService.delete(id).subscribe((res) => {
+      this.mainDatas$ = this.mainDatas$.filter((item) => item.itemno !== id);
+      console.log('Post deleted successfully!');
+    });
   }
 
   //Filter
@@ -98,20 +126,27 @@ export class StockComponent implements OnInit {
         let found = false;
         if (isFilterSet) {
           for (const col in searchTerms) {
-            searchTerms[col].trim().toLowerCase().split(' ').forEach((word: any) => {
-              if (data[col].toString().toLowerCase().indexOf(word) != -1 && isFilterSet) {
-                found = true
-              }
-            });
+            searchTerms[col]
+              .trim()
+              .toLowerCase()
+              .split(' ')
+              .forEach((word: any) => {
+                if (
+                  data[col].toString().toLowerCase().indexOf(word) != -1 &&
+                  isFilterSet
+                ) {
+                  found = true;
+                }
+              });
           }
-          return found
+          return found;
         } else {
           return true;
         }
-      }
-      return nameSearch()
-    }
-    return filterFunction
+      };
+      return nameSearch();
+    };
+    return filterFunction;
   }
 
   // Get Uniqu values from columns to build filter
@@ -129,13 +164,16 @@ export class StockComponent implements OnInit {
   // Called on Filter change
   filterChange(filter: any, event: any) {
     //let filterValues = {}
-    this.filterValues[filter.columnProp] = event.target.value.trim().toLowerCase()
-    this.dataSource.filter = JSON.stringify(this.filterValues)
+    this.filterValues[filter.columnProp] = event.target.value
+      .trim()
+      .toLowerCase();
+    this.dataSource.filter = JSON.stringify(this.filterValues);
   }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    console.log(this.dataSource.filter);
     //this.dataSource.filter = JSON.stringify(filterValue.trim().toLowerCase())
 
     if (this.dataSource.paginator) {
@@ -143,7 +181,7 @@ export class StockComponent implements OnInit {
     }
   }
 
-  testConsoleLog(data: any){
-    console.log(data)
+  testConsoleLog(data: any) {
+    console.log(data);
   }
 }

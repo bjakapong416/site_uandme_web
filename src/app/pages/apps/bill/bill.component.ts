cdb6bd441f0af5c414bf2330c587aecc8f2d5248
chunosuke <ch.chunspringz@gmail.com>
2022-05-08 06:56:12 +0700

    filter: month & year - BILL

--- src/app/pages/apps/bill/bill.component.ts
+++ src/app/pages/apps/bill/bill.component.ts
@@ -5,21 +5,53 @@ import { BillService } from '../../_services/bill/bill.service';
 import { AuthService } from 'src/app/modules/auth';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatDatepicker } from '@angular/material/datepicker';
 import { getCSSVariableValue } from 'src/app/_metronic/kt/_utils';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { DetailBillComponent } from './detail-bill/detail-bill/detail-bill.component';
 import { AddBillComponent } from './add-bill/add-bill/add-bill.component';
+import { NativeDateAdapter, DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
+import { formatDate } from '@angular/common';
+import { DatePipe } from '@angular/common';
+
+export const PICK_FORMATS = {
+  parse: {dateInput: {month: 'short', year: 'numeric', day: 'numeric'}},
+  display: {
+      dateInput: 'input',
+      monthYearLabel: {year: 'numeric', month: 'short'},
+      dateA11yLabel: {year: 'numeric', month: 'long', day: 'numeric'},
+      monthYearA11yLabel: {year: 'numeric', month: 'long'}
+  }
+};
+
+class PickDateAdapter extends NativeDateAdapter {
+  format(date: Date, displayFormat: Object): string {
+      if (displayFormat === 'input') {
+          return formatDate(date,'yyyy-MM',this.locale);
+      } else {
+          return date.toDateString();
+      }
+  }
+}
 
 @Component({
   selector: 'app-bill',
   templateUrl: './bill.component.html',
   styleUrls: ['./bill.component.scss'],
+  providers: [
+    {provide: DateAdapter, useClass: PickDateAdapter},
+    {provide: MAT_DATE_FORMATS, useValue: PICK_FORMATS}
+  ]
 })
+
 export class BillComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  pipe = new DatePipe('en-US');
+  filterValues: any = {"all":"","date":""};
   @Input() chartColor: string = '';
   @Input() chartHeight: string;
   chartOptions: any = {};
+  pickDateValue: any;
 
   displayedColumns: string[] = [
     'name_cus',
@@ -51,6 +83,21 @@ export class BillComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
   }
 
+  // Called on Filter change
+  filterChange(filter: any, event: any) {
+    //let filterValues = {}
+    if(filter == 'date') {
+      let date = event.target.value;
+      this.filterValues[filter] = this.pipe.transform(date, 'yyyy-MM') || '';
+      console.log('YES!!');
+    } else {
+      this.filterValues[filter] = event.target.value
+        .trim()
+        .toLowerCase();
+    }
+    this.dataSource.filter = JSON.stringify(this.filterValues);
+  }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -64,6 +111,7 @@ export class BillComponent implements OnInit {
     this.billService.getAll().subscribe((data: any) => {
       this.dataSource = new MatTableDataSource(data);
       this.dataSource.paginator = this.paginator;
+      this.dataSource.filterPredicate = this.NEW_createFilter();
       console.log(this.dataSource);
     });
   }
@@ -73,6 +121,20 @@ export class BillComponent implements OnInit {
       this.mainDatas$ = this.mainDatas$.filter((item) => item.docnum !== id);
       console.log('Post deleted successfully!');
     });
+  }
+
+  NEW_createFilter() {
+    let filterFunction = function (data: any, filter: string): boolean {
+      let searchTerms = JSON.parse(filter);
+      let textSearch = '';
+      for (var key in data) {
+        textSearch += data[key];
+      }
+      console.log(filter);
+      let searchStr = textSearch.toLowerCase();
+      return searchStr.indexOf(searchTerms.all.toLowerCase()) != -1 && data.docdat.toLowerCase().indexOf(searchTerms.date) !== -1;
+    };
+    return filterFunction;
   }
 
   //action
@@ -90,6 +152,14 @@ export class BillComponent implements OnInit {
       windowClass: 'bg-blank',
     });
     modalRef.componentInstance.id = id;
+  }
+
+  setMonthYear(event: Event, datepicker: MatDatepicker<any>) {
+    let date = event.toString();
+    this.pickDateValue = event;
+    this.filterValues['date'] = this.pipe.transform(date, 'yyyy-MM') || '';
+    this.dataSource.filter = JSON.stringify(this.filterValues);
+    datepicker.close();
   }
 }
 
